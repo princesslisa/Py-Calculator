4181639ddc09643f22cdcd69064816e70d53f27d
princesslisa <lisaogbechie@gmail.com>
2022-05-22 14:55:19 +0100

    Added comments

--- Functions.py
+++ Functions.py
@@ -3,13 +3,18 @@ while True:
     b = input("Enter numbers(please separate with commas): ")
     sep = b.split(",")  # Separate the entered numbers into a list
 
+    # Checks if the input is all digits or if there is a letter in between
     x = b.replace(",", "").replace(".", "")
     valid = all(i.isdigit() for i in x)
     print(x)
 
     if valid:
+        # Converts the numbers to floats for calculation
+        # Not converting to integers, in the event that there is a float in the entries
         a = [round(float(i), 3) for i in sep]
-        ops = input("Enter operator: ")
+
+        # Checks for the operator sign and carries out operation
+        ops = input('Enter operator: ')
         if ops == "-":
             c = a[0]
             i = 1
@@ -37,6 +42,7 @@ while True:
         print("Therefore,", b.replace(",", ops), "= ", c)
         break
 
+    #If there is a non-digit character in the entries, it writes out the entries and asks you to enter input again
     else:
         print("There is a non-digit in your numbers:", sep)
         continue
